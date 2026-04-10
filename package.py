402f4d5dc9e122d556ffd5904ee981f20c1c4db4
rhdutta <rdutta@halon.com>
2026-04-10 13:55:07 +0100

    Strip dev label for release
    - Remove +shotcomp label from version (0.2.18+shotcomp → 0.2.18)

--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "aftereffects"
 title = "AfterEffects"
-version = "0.2.18+shotcomp"
+version = "0.2.18"
 app_host_name = "aftereffects"
 client_dir = "ayon_aftereffects"
 project_can_override_addon_version = True
