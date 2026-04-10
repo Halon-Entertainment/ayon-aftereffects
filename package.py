03b8d2d9dcc0a752eb2444e2551d82559b32e8a2
rhdutta <rdutta@halon.com>
2026-04-10 19:25:11 +0100

    Strip dev label for release
    - Remove +versionup label from version 0.2.19

--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "aftereffects"
 title = "AfterEffects"
-version = "0.2.19+versionup"
+version = "0.2.19"
 app_host_name = "aftereffects"
 client_dir = "ayon_aftereffects"
 project_can_override_addon_version = True
