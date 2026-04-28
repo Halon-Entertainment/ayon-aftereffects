2c9510f0d8d296a47b6b5b7d17013e3f0c6c3fc3
rhdutta <rdutta@halon.com>
2026-04-28 17:30:18 +0100

    Strip version label for release
    - 0.2.20+comp → 0.2.20

--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "aftereffects"
 title = "AfterEffects"
-version = "0.2.20+comp"
+version = "0.2.20"
 app_host_name = "aftereffects"
 client_dir = "ayon_aftereffects"
 project_can_override_addon_version = True
