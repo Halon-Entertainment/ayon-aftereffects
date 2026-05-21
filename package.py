947f89b217d74a5a79b7ec8c90f0bec163658642
rhdutta <rdutta@halon.com>
2026-05-21 20:00:53 +0100

    Strip version label for release
    - 1.0.6+saveas → 1.0.6

--- package.py
+++ package.py
@@ -1,7 +1,7 @@
 name = "aftereffects"
 title = "AfterEffects"
 # Halon fork of ynput/ayon-aftereffects (forked from 0.2.12)
-version = "1.0.6+saveas"
+version = "1.0.6"
 app_host_name = "aftereffects"
 client_dir = "ayon_aftereffects"
 project_can_override_addon_version = True
